8bcdbd5bf1dbb4d22c0b555fd6ff8ad6f5f8a0b2
JackieLin2004 <linxy20040304@163.com>
2024-12-22 21:57:41 +0800

    Basic environment configuration

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import './assets/main.css'
+import '@/assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
@@ -6,9 +6,17 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
+import axios from "axios";
+
+axios.defaults.baseURL = "http://localhost:8088";
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(ElementPlus)
 
 app.mount('#app')
