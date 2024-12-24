7208ecb85546e11d159614e0dda82bc642c1da79
JackieLin2004 <linxy20040304@163.com>
2024-12-25 03:22:48 +0800

    Complete the joint debugging of the reset

--- src/views/ResetPage.vue
+++ src/views/ResetPage.vue
@@ -1,18 +1,90 @@
 <script setup lang="ts">
-import {ref} from "vue";
+import {ref, reactive} from "vue";
 import {EditPen, Lock, Message} from "@element-plus/icons-vue";
 import router from "@/router";
 import {ElMessage} from "element-plus";
+import {get, post} from "@/net/index.ts";
 
-const active =ref(0)
+const active = ref(0)
+const formRef = ref()
+const coldTime = ref(0)
 
-const confirmReset = ()=> {
-  active.value++
+const form = reactive({
+  email: '',
+  code: '',
+  password: '',
+  password_repeat: ''
+})
+
+const validatePassword = (rules, value, callback) => {
+  if (value === '') {
+    callback(new Error('请再次输入密码'))
+  } else if (value !== form.password) {
+    callback(new Error("两次输入的密码不一致"))
+  } else {
+    callback()
+  }
 }
 
-const doReset = ()=> {
-  router.push('/login')
-  ElMessage.success('密码重置成功')
+const rules = {
+  email: [
+    {required: true, message: '请输入邮件地址', trigger: 'blur'},
+    {type: 'email', message: '请输入合法的电子邮件地址', trigger: ['blur', 'change']}
+  ],
+  code: [
+    {required: true, message: '请输入获取的验证码', trigger: 'blur'},
+  ],
+  password: [
+    {required: true, message: '请输入密码', trigger: 'blur'},
+    {min: 6, max: 20, message: '密码的长度必须在6-20个字符之间', trigger: ['blur']}
+  ],
+  password_repeat: [
+    {validator: validatePassword, trigger: ['blur', 'change']},
+  ],
+}
+
+const askCode = () => {
+  if (isEmailValid) {
+    coldTime.value = 60
+    get(`/auth/ask-code?email=${form.email}&type=reset`, () => {
+      ElMessage.success(`验证码已发送到邮箱: ${form.email}，请注意查收`)
+      const handle = setInterval(() => {
+        coldTime.value--
+        if (coldTime.value === 0) {
+          clearInterval(handle)
+        }
+      }, 1000)
+    }, (message) => {
+      ElMessage.warning(message)
+      coldTime.value = 0
+    })
+  } else {
+    ElMessage.warning("请输入正确的电子邮件！")
+  }
+}
+
+const isEmailValid = computed(() => /^[\w.-]+@[\w.-]+\.\w+$/.test(form.email))
+
+const confirmReset = () => {
+  formRef.value.validate((isValid) => {
+    if (isValid) {
+      post('/auth/reset-confirm', {
+        email: form.email,
+        code: form.code
+      }, () => active.value++)
+    }
+  })
+}
+
+const doReset = () => {
+  formRef.value.validate((isValid) => {
+    if (isValid) {
+      post('/auth/reset-password', {...form}, () => {
+        ElMessage.success('密码重置成功，请重新登录')
+        router.push('/login')
+      })
+    }
+  })
 }
 </script>
 
@@ -34,9 +106,9 @@ const doReset = ()=> {
           <div class="text-[1em] mt-[8px] text-gray-500">请输入需要重置密码的电子邮件地址</div>
         </div>
         <div class="mt-[5vw]">
-          <el-form>
+          <el-form :model="form" :rules="rules" ref="formRef">
             <el-form-item prop="email">
-              <el-input type="email" placeholder="电子邮件地址">
+              <el-input v-model="form.email" type="email" placeholder="电子邮件地址">
                 <template #prefix>
                   <el-icon>
                     <Message/>
@@ -47,7 +119,7 @@ const doReset = ()=> {
             <el-form-item prop="code">
               <el-row :gutter="10" class="w-full">
                 <el-col :span="16">
-                  <el-input :maxlength="6" type="text" placeholder="请输入验证码">
+                  <el-input v-model="form.code" :maxlength="6" type="text" placeholder="请输入验证码">
                     <template #prefix>
                       <el-icon>
                         <EditPen/>
@@ -56,7 +128,9 @@ const doReset = ()=> {
                   </el-input>
                 </el-col>
                 <el-col :span="6">
-                  <el-button type="success">获取验证码</el-button>
+                  <el-button @click="askCode" :disabled="!isEmailValid || coldTime > 0" type="success">
+                    {{ coldTime > 0 ? `请稍后 ${coldTime} 秒` : '获取验证码' }}
+                  </el-button>
                 </el-col>
               </el-row>
             </el-form-item>
@@ -72,9 +146,9 @@ const doReset = ()=> {
           <div class="text-[1em] mt-[8px] text-gray-500">请填写你的新密码，并牢记防止丢失</div>
         </div>
         <div class="mt-[5vw]">
-          <el-form>
+          <el-form :model="form" :rules="rules" ref="formRef">
             <el-form-item prop="password">
-              <el-input maxlength="20" type="password" placeholder="密码">
+              <el-input v-model="form.password" maxlength="20" type="password" placeholder="密码">
                 <template #prefix>
                   <el-icon>
                     <Lock/>
@@ -83,7 +157,7 @@ const doReset = ()=> {
               </el-input>
             </el-form-item>
             <el-form-item prop="password_repeat">
-              <el-input maxlength="20" type="password" placeholder="重复密码">
+              <el-input v-model="form.password_repeat" maxlength="20" type="password" placeholder="重复密码">
                 <template #prefix>
                   <el-icon>
                     <Lock/>
