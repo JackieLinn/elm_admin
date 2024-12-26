057a5ccbdc6c16ee818d875170785a077b622093
JackieLin2004 <linxy20040304@163.com>
2024-12-26 16:38:56 +0800

    Completion the footer part

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,25 @@
 
 <template>
   <RouterView></RouterView>
+
+  <div class="w-full h-[14vw] border-t border-[#DDD] fixed left-0 bottom-0 flex justify-around items-center bg-white">
+    <div class="text-center">
+      <i-material-symbols-house-rounded class="text-[#888] text-[6vw] ml-[0.5vw]"/>
+      <span class="text-[#888] text-[4vw] font-medium">首页</span>
+    </div>
+    <div class="text-center">
+      <i-tdesign-compass class="text-[#888] text-[6vw] ml-[0.5vw]"/>
+      <span class="text-[#888] text-[4vw] font-medium">发现</span>
+    </div>
+    <div class="text-center">
+      <i-material-symbols-text-snippet-outline-sharp class="text-[#888] text-[6vw] ml-[0.5vw]"/>
+      <span class="text-[#888] text-[4vw] font-medium">订单</span>
+    </div>
+    <div class="text-center">
+      <i-ic-round-person class="text-[#888] text-[6vw] ml-[0.5vw]"/>
+      <span class="text-[#888] text-[4vw] font-medium">我的</span>
+    </div>
+  </div>
 </template>
 
 <style scoped>
