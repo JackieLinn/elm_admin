2a03c662e9c25c58bd3db30ca1d4343e5d69fe99
JackieLin2004 <linxy20040304@163.com>
2024-12-26 16:02:02 +0800

    Completion the banner part of HomePage

--- components.d.ts
+++ components.d.ts
@@ -7,6 +7,7 @@ export {}
 /* prettier-ignore */
 declare module 'vue' {
   export interface GlobalComponents {
+    Banner: typeof import('./src/components/Home/Banner.vue')['default']
     ElButton: typeof import('element-plus/es')['ElButton']
     ElCheckbox: typeof import('element-plus/es')['ElCheckbox']
     ElCol: typeof import('element-plus/es')['ElCol']
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -2,12 +2,14 @@
 import HomeHeader from "@/components/Home/HomeHeader.vue";
 import Search from "@/components/Home/Search.vue";
 import FoodCategory from "@/components/Home/FoodCategory.vue";
+import Banner from "@/components/Home/Banner.vue";
 </script>
 
 <template>
   <HomeHeader/>
   <Search/>
   <FoodCategory/>
+  <Banner/>
 </template>
 
 <style scoped>
